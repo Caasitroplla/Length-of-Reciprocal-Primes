3ed48b797d29e3b2f284268b2be929473d7ff7c2
Isaac Allport <48483898+Caasitroplla@users.noreply.github.com>
2022-04-04 18:14:01 +0000

    Saving all previous calculations

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 from src.prime import get_primes
-from src.json_module import save_data
+from src.json_module import save_data, get_data
 from src.recursive_reciprocal import find_len_repeating_reciprocal
 
 def main():
+	# Getting the previous calculations from json
+	dict = get_data()
 	# The primes are calculated here between the upper and lower value
-	lower = 0
-	upper = 99
+	lower = dict['data']['end_point']
+	upper = lower + 300
 	primes = get_primes(lower, upper)
-	# Dict to store values
-	dict = { "primes" : [] }
 	# Calculates the number of values 
 	for prime in primes:
 		dict["primes"].append({"prime":prime, "len_reciprocal_root": find_len_repeating_reciprocal(prime)})
+	# Setting new upper value
+	dict['data']['end_point'] = upper
+	# Setting number of values done
+	dict['data']['values_completed'] = len(dict['primes'])
 	# Save those values to json
 	save_data(dict)
 
